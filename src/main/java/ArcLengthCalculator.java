e591465d00b166b108d183739a7e920ebec8fd91
jacobkobr <jkobrick6@gmail.com>
2024-10-20 10:13:47 -0700

    update

--- src/main/java/ArcLengthCalculator.java
+++ src/main/java/ArcLengthCalculator.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+import java.util.List;
 
 public class ArcLengthCalculator {
+
+    public static double computeTotalArcLength(List<Main.FunctionBounds> functions) {
+        double totalLength = 0;
+        for (Main.FunctionBounds fb : functions) {
+            totalLength += computeArcLength(fb.function, fb.lower, fb.upper);
+        }
+        return totalLength;
+    }
+
+    private static double computeArcLength(String function, double lower, double upper) {
+        Expression expr = new ExpressionBuilder(function).variable("x").build();
+        double totalLength = 0;
+        double step = 0.01;
+
+        for (double x = lower; x < upper; x += step) {
+            double yAtX = expr.setVariable("x", x).evaluate();
+            double yAtNextX = expr.setVariable("x", x + step).evaluate();
+            double dy = yAtNextX - yAtX;
+            double segmentLength = Math.sqrt(step * step + dy * dy);
+            totalLength += segmentLength;
+        }
+
+        return totalLength;
+    }
 }
+
--- src/main/java/Surface3D.java
+++ src/main/java/Surface3D.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Cylinder;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+import java.util.List;
 
-public class Surface3D {
+public class Surface3D extends Group {
+
+    public Surface3D(List<Main.FunctionBounds> functions, String axis) {
+        for (Main.FunctionBounds fb : functions) {
+            for (double x = fb.lower; x < fb.upper; x += 0.1) {
+                double radius = evaluateFunction(fb.function, x);
+                Cylinder segment = new Cylinder(radius * 10, 1);
+                segment.setMaterial(new PhongMaterial(Color.GREEN));
+
+                if (axis.equals("x-axis")) {
+                    segment.setTranslateX(x * 10);
+                    segment.setRotate(90);
+                } else {
+                    segment.setTranslateY(x * 10);
+                }
+
+                this.getChildren().add(segment);
+            }
+        }
+    }
+
+    private double evaluateFunction(String function, double x) {
+        Expression expr = new ExpressionBuilder(function).variable("x").build();
+        return expr.setVariable("x", x).evaluate();
+    }
 }
